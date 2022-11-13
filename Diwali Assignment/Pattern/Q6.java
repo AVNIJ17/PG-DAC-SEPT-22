4075042a5536df2ae3aa7db448205d1fd0479b06
AVNIJ17 <113444922+AVNIJ17@users.noreply.github.com>
2022-11-13 08:31:51 +0530

    Add files via upload

---
+++ Diwali Assignment/Pattern/Q6.java
@@ -0,0 +1,20 @@
+//Q6
+
+import java.lang.*;
+import java.util.*;
+
+class Q6{
+	
+	public static void main(String args[]){
+	
+		for(int i = 1; i <= 5; i++){
+			
+			for(int j = i; j >= 1; j--){
+				
+				System.out.print(j+" ");
+			}
+			
+			System.out.println();
+		}
+	}
+}
